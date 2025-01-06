b82ad4bd1dc8ee76c0ff0e183c37c1ca5c4fd930
victor-huallpa <vech.600@gmail.com>
2025-01-05 22:11:30 -0500

    POO

--- app/views/content/dashboard-view.php
+++ app/views/content/dashboard-view.php
@@ -1,1 +1,11 @@
-home
+<div class="container is-fluid">
+	<h1 class="title">Home</h1>
+  	<div class="columns is-flex is-justify-content-center">
+    	<figure class="image is-128x128">
+    		<img class="is-rounded" src="<?php echo APP_URL; ?>app/views/resources/photos/default.png">
+		</figure>
+  	</div>
+  	<div class="columns is-flex is-justify-content-center">
+  		<h2 class="subtitle">¡Bienvenido User Name!</h2>
+  	</div>
+</div>
--- app/views/inc/layouts/footer.php
+++ app/views/inc/layouts/footer.php
@@ -1,3 +1,12 @@
-<footer>
-    piede de pagina
+<footer class="footer">
+    <p class="footer-text">
+        &copy; <span id="current-year"></span> VECHTECNOLOGY. Todos los derechos reservados.
+    </p>
+    <p class="footer-links">
+        <a href="politica_privacidad.html">Política de Privacidad</a> | 
+        <a href="terminos_servicio.html">Términos de Servicio</a>
+    </p>
 </footer>
+<script>
+    document.getElementById("current-year").textContent = new Date().getFullYear();
+</script>
