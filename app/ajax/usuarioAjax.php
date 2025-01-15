3c00b4e5fb590c21f9f68c42e891fec92ec09387
victor-huallpa <vech.600@gmail.com>
2025-01-14 20:50:07 -0500

    CONTROLLER USER

--- app/ajax/usuarioAjax.php
+++ app/ajax/usuarioAjax.php
@@ -2,3 +2,16 @@
     require_once '../../config/app.php';
     require_once '../views/inc/session_start.php';
     require_once '../../autoload.php';
+
+    use app\controllers\userController;
+
+    if(isset($_POST['modulo_usuario'])){
+        $inUsuario = new userController();
+
+        if($_POST['modulo_usuario'] == 'registrar'){
+            echo $inUsuario->regsitrarUsuarioControlador();
+        }else{}
+    }else{
+        session_destroy();
+        header('Location: '.APP_URL.'login/');
+    }
--- app/controllers/userController.php
+++ app/controllers/userController.php
@@ -3,5 +3,77 @@
     use app\models\mainModel;
 
     class userController extends mainModel{
-        
+
+        #metodo de registar un usuario
+        public function regsitrarUsuarioControlador(){
+            #almacenar los datos del usuario
+            $nombre = $this->limpiarCadena($_POST['usuario_nombre']);
+            $apellido = $this->limpiarCadena($_POST['usuario_apellido']);
+            $usuairo = $this->limpiarCadena($_POST['usuario_usuario']);
+            $email = $this->limpiarCadena($_POST['usuario_email']);
+            $clave1 = $this->limpiarCadena($_POST['usuario_clave_1']);
+            $clave2 = $this->limpiarCadena($_POST['usuario_clave_2']);
+
+            #verificando campos obligatorios
+            if($nombre == '' || $apellido == '' || $usuairo == '' || $clave1 == '' || $clave2 == ''){
+                $alerta = [
+                    "tipo" => 'simple',
+                    "titulo" => "Ocurrio un error Inerperado",
+                    "texto" => "No as llenado todo los campo que son obligatorios",
+                    "icono" => 'error'
+                ];
+
+                return json_encode($alerta);
+                exit();
+            }
+
+            #verificar la integridad de los datos
+            if($this->verificarDatos("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{3,40}", $nombre)){
+                $alerta = [
+                    "tipo" => 'simple',
+                    "titulo" => "Ocurrio un error Inerperado",
+                    "texto" => "El nombre no coinside con los caracteres solicitados",
+                    "icono" => 'error'
+                ];
+
+                return json_encode($alerta);
+                exit();
+            }
+
+            if($this->verificarDatos("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{3,40}", $apellido)){
+                $alerta = [
+                    "tipo" => 'simple',
+                    "titulo" => "Ocurrio un error Inerperado",
+                    "texto" => "El apellido no coinside con los caracteres solicitados",
+                    "icono" => 'error'
+                ];
+
+                return json_encode($alerta);
+                exit();
+            }
+
+            if($this->verificarDatos("[a-zA-Z0-9]{4,20}", $usuairo)){
+                $alerta = [
+                    "tipo" => 'simple',
+                    "titulo" => "Ocurrio un error Inerperado",
+                    "texto" => "El nombre de usuario no coinside con los caracteres solicitados",
+                    "icono" => 'error'
+                ];
+
+                return json_encode($alerta);
+                exit();
+            }
+
+            if($this->verificarDatos("[a-zA-Z0-9$@.-]{7,100}", $clave1) || $this->verificarDatos("[a-zA-Z0-9$@.-]{7,100}", $clave2)){
+                $alerta = [
+                    "tipo" => 'simple',
+                    "titulo" => "Ocurrio un error Inerperado",
+                    "texto" => "Las claves no coinside con los caracteres solicitados",
+                    "icono" => 'error'
+                ];
+
+                return json_encode($alerta);
+                exit();
+            }
+        }
     }
